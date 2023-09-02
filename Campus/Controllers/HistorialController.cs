5dd4ba7e97adaaa3ea6179e1d39753106de5dc69
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-09-02 10:21:00 -0400

    Creando nuevo proyecto

---
+++ Campus/Controllers/HistorialController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Campus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistorialController : ControllerBase
+    {
+        public HistorialController() { }
+        [HttpPost]
+        public ActionResult Post()
+        {
+            Console.WriteLine("Llegó una petición al servicio Campus/Historial");
+            return Ok("Respuesta exitosa desde el controlador Historial");
+        }
+    }
+}
